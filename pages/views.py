c2f68fca54c4431192450fd81fdfa6d6bc233e0e
bhuwan <bhuwanneupane@bajratechnologies.com>
2021-08-25 21:20:01 +0545

    featured car and all car is added to the frontend

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,12 @@ from .models import *
 # Create your views here.
 def home(request):
     teams = Teams.objects.all()
-    context = {'teams':teams}
+    featured_car = Car.objects.filter(if_featured=True).order_by('-created_date')
+    all_car = Car.objects.order_by('-created_date')
+    context = {'teams':teams,
+                'featured_car': featured_car, 
+                'all_car': all_car,
+    }
     return render(request, 'pages/home.html',context )
 
 def contact(request):
